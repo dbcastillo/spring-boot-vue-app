33a8915ae12de3693ad6f6b9ff2742d25b7f6ef1
Derick Castillo <derick.castillo@daveramsey.com>
2025-03-24 05:49:43 -0500

    Update controller and repository for errors

--- src/main/java/com/example/employeebackend/controller/EmployeeController.java
+++ src/main/java/com/example/employeebackend/controller/EmployeeController.java
@@ -2,12 +2,19 @@ package com.example.employeebackend.controller;
 
 import com.example.employeebackend.entity.Employee;
 import com.example.employeebackend.service.EmployeeService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/employees")
@@ -15,7 +22,6 @@ import java.util.List;
 @CrossOrigin(origins = "http://localhost:5173")
 public class EmployeeController {
     private final EmployeeService employeeService;
-
 
     @GetMapping
     public ResponseEntity<List<Employee>> getEmployees(){
@@ -41,7 +47,7 @@ public class EmployeeController {
         return new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
     }
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> deleteEmployee(@PathVariable Long id){
+    public ResponseEntity<q> deleteEmployee(@PathVariable Long id){
         employeeService.deleteEmployee(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }

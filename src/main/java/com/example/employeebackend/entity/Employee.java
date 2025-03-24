c9380aa03ea91c225a3d820868ba66a344e93ce0
Derick Castillo <derick.castillo@daveramsey.com>
2025-03-23 22:39:10 -0500

    Setup employee entity file

---
+++ src/main/java/com/example/employeebackend/entity/Employee.java
@@ -0,0 +1,29 @@
+package com.example.employeebackend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Employee  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "First_name", nullable = false)
+    private String firstname;
+    @Column(name = "Last_name", nullable = false)
+    private String lastname;
+    private String email;
+}

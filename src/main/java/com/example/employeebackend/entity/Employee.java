598b853a072c180fe94873a94a1aa694b3b69172
Derick Castillo <derick.castillo@daveramsey.com>
2025-03-31 12:23:10 -0500

    Update employee entit for employee-backend

--- src/main/java/com/example/employeebackend/entity/Employee.java
+++ src/main/java/com/example/employeebackend/entity/Employee.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,6 +17,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Data
 @Entity
 public class Employee  {
     @Id
